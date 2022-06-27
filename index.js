5535f9568835b05c9c6995a35df04e3074bf347e
Kushagra0214 <89725297+Kushagra0214@users.noreply.github.com>
2022-06-27 17:02:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,279 @@
+const data = {
+    "_key": "topic:1053",
+    "cid": null,
+    "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop p",
+    "globalTags": [],
+    "lastposttime": 0,
+    "learning_outcomes": [
+        "learning outcome 1",
+        "learning outcome 2",
+        "learning outcome 3"
+    ],
+    "mainPid": 0,
+    "postcount": 0,
+    "pre_requisites": [
+        "pre-requisites 1",
+        "pre-requisites 2",
+        "pre-requisites 3"
+    ],
+    "project_image": "https://media.giphy.com/media/l3vR85PnGsBwu1PFK/giphy.gif",
+    "short_description": "",
+    "slug": "1053/new-project",
+    "start_time": 1653517339588,
+    "status": "published",
+    "tasks": [
+        {
+            "task_id": 6233,
+            "task_title": "demo task",
+            "task_description": "desc desc",
+            "tools": [],
+            "assets": [
+                {
+                    "asset_id": 6234,
+                    "asset_title": "Task Heading 1",
+                    "asset_description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type a",
+                    "asset_type": "display_asset",
+                    "asset_content": "reflection",
+                    "display_asset_url": "",
+                    "display_asset_image": "",
+                    "display_asset_video": "",
+                    "display_asset_docs": "",
+                    "display_tb_tid": null,
+                    "display_tb_pid": null,
+                    "display_eb_tid": null,
+                    "display_eb_pid": null,
+                    "display_asset_reflection": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop "
+                },
+                {
+                    "asset_id": 6235,
+                    "asset_title": "Task Heading 2",
+                    "asset_description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
+                    "asset_type": "input_asset",
+                    "asset_content": "tb",
+                    "display_asset_url": null,
+                    "display_asset_image": null,
+                    "display_asset_video": null,
+                    "display_asset_docs": null,
+                    "display_tb_tid": null,
+                    "display_tb_pid": null,
+                    "display_eb_tid": null,
+                    "display_eb_pid": null,
+                    "display_asset_reflection": null
+                },
+                {
+                    "asset_id": 6236,
+                    "asset_title": "Task Heading 3",
+                    "asset_description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
+                    "asset_type": "input_asset",
+                    "asset_content": "reflection",
+                    "display_asset_url": null,
+                    "display_asset_image": null,
+                    "display_asset_video": null,
+                    "display_asset_docs": null,
+                    "display_tb_tid": null,
+                    "display_tb_pid": null,
+                    "display_eb_tid": null,
+                    "display_eb_pid": null,
+                    "display_asset_reflection": null
+                },
+                {
+                    "asset_id": 6237,
+                    "asset_title": "Task Heading 4",
+                    "asset_description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
+                    "asset_type": "display_asset",
+                    "asset_content": "other",
+                    "display_asset_url": "",
+                    "display_asset_image": "",
+                    "display_asset_video": "",
+                    "display_asset_docs": "https://drive.google.com/file/d/1Pxw83bTKE3KkOyYnwm1u6DSVjntSrNTp/preview?usp=sharing",
+                    "display_tb_tid": null,
+                    "display_tb_pid": null,
+                    "display_eb_tid": null,
+                    "display_eb_pid": null,
+                    "display_asset_reflection": null
+                },
+                {
+                    "asset_id": 6238,
+                    "asset_title": "Task Heading 5",
+                    "asset_description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
+                    "asset_type": "display_asset",
+                    "asset_content": "other",
+                    "display_asset_url": "",
+                    "display_asset_image": "https://media.giphy.com/media/IzjhI7ggjDlEnMxZMu/giphy.gif",
+                    "display_asset_video": "",
+                    "display_asset_docs": "",
+                    "display_tb_tid": null,
+                    "display_tb_pid": null,
+                    "display_eb_tid": null,
+                    "display_eb_pid": null,
+                    "display_asset_reflection": null
+                },
+                {
+                    "asset_id": 6239,
+                    "asset_title": "Task Heading 6",
+                    "asset_description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
+                    "asset_type": "input_asset",
+                    "asset_content": "article",
+                    "display_asset_url": null,
+                    "display_asset_image": null,
+                    "display_asset_video": null,
+                    "display_asset_docs": null,
+                    "display_tb_tid": null,
+                    "display_tb_pid": null,
+                    "display_eb_tid": null,
+                    "display_eb_pid": null,
+                    "display_asset_reflection": null
+                },
+                {
+                    "asset_id": 6240,
+                    "asset_title": "Task Heading 7",
+                    "asset_description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
+                    "asset_type": "input_asset",
+                    "asset_content": "eb",
+                    "display_asset_url": null,
+                    "display_asset_image": null,
+                    "display_asset_video": null,
+                    "display_asset_docs": null,
+                    "display_tb_tid": null,
+                    "display_tb_pid": null,
+                    "display_eb_tid": null,
+                    "display_eb_pid": null,
+                    "display_asset_reflection": null
+                },
+                {
+                    "asset_id": 6241,
+                    "asset_title": "Task Heading 8",
+                    "asset_description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
+                    "asset_type": "display_asset",
+                    "asset_content": "other",
+                    "display_asset_url": "https://drive.google.com/file/d/1dBBYQsz2yt3vc4YMkRmVC3nuTBbq2GON/preview?usp=sharing",
+                    "display_asset_image": "",
+                    "display_asset_video": "",
+                    "display_asset_docs": "",
+                    "display_tb_tid": null,
+                    "display_tb_pid": null,
+                    "display_eb_tid": null,
+                    "display_eb_pid": null,
+                    "display_asset_reflection": null
+                },
+                {
+                    "asset_id": 6242,
+                    "asset_title": "Task Heading 9",
+                    "asset_description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
+                    "asset_type": "display_asset",
+                    "asset_content": "other",
+                    "display_asset_url": "",
+                    "display_asset_image": "",
+                    "display_asset_video": "https://www.youtube.com/embed/s8H0Sx7jy7c",
+                    "display_asset_docs": "",
+                    "display_tb_tid": null,
+                    "display_tb_pid": null,
+                    "display_eb_tid": null,
+                    "display_eb_pid": null,
+                    "display_asset_reflection": null
+                }
+            ]
+        }
+    ],
+    "tid": 1053,
+    "timestamp": 1653516431690,
+    "title": "new Project",
+    "tools": [],
+    "type": "project",
+    "uid": 100,
+    "viewcount": 0
+}
+
+for(var i=0; i<data.tasks[0].assets.length; i++){
+    var doc = document.getElementsByClassName('tasks')[0].innerHTML;
+    var path = data.tasks[0].assets[i];
+    var title = path.asset_title;
+    var description = path.asset_description;
+    var type = path.asset_type;
+    var content = path.asset_content;
+    var url = path.display_asset_url;
+    var video = path.display_asset_video;
+    var image = path.display_asset_image;
+    var docs = path.display_asset_docs;
+    var reflection = path.display_asset_reflection;
+
+    var toRender = "";
+    if(type=="input_asset"){
+        var textarea25_1 = '';
+        var textarea25_2 = '';
+        var textarea50 = '';
+        var textarea75 = '';
+
+        if(content=="tb"){
+
+            textarea25_1 = '<div class="textarea-input form-outline w-25 mb-4"><label class="form-label" for="textAreaExample4">Sub Thread 1</label><textarea class="form-control" id="textAreaExample4" rows="3" placeholder="Enter Text Here"></textarea></div>'
+            textarea25_2 = '<div class="textarea-input form-outline w-25 mb-4"><label class="form-label" for="textAreaExample4">Sub Interpolation 1</label><textarea class="form-control" id="textAreaExample4" rows="3" placeholder="Enter Text Here"></textarea></div>'
+            textarea75 = '<div class="textarea-input form-outline w-75 mb-4"><label class="form-label" for="textAreaExample4">Summary for thread A</label><textarea class="form-control" id="textAreaExample4" rows="3" placeholder="Enter Text Here"></textarea></div>'
+            icon_bulb = '<div class="dropdown-icons"><i class="fa-solid fa-lightbulb"></i></div>';
+            icon_msg = '<div class="dropdown-icons"><i class="fa-solid fa-message"></i></div>';
+            dropbox1 = '<select name = "dropdown"><option value = "category" selected>Select Category</option><option value = "html">HTML</option><option value = "css">CSS</option></option><option value = "js">Javascript</option></select>'
+            dropbox2 = '<select name = "dropdown"><option value = "process" selected>Select Process</option><option value = "html">HTML</option><option value = "css">CSS</option></option><option value = "js">Javascript</option></select>'
+            dropbox3 = '<select name = "dropdown"><option value = "emotion" selected>Select Emotion</option><option value = "html">HTML</option><option value = "css">CSS</option></option><option value = "js">Javascript</option></select>'
+            button1 = '<button class="btn btn-primary btn-sm sub-thread-btn">+ Sub Thread</button>'
+            button2 = '<button class="btn btn-primary btn-new-thread-tb">+ New Thread</button>'
+            toRender = '<center>'+textarea25_1 + textarea25_2 + '<br>' + dropbox1 + dropbox2 + icon_bulb + icon_msg + button1 + textarea75 + dropbox3 + '<br><br><br>' + button2 +'</center><br><br>'; 
+        }
+        if(content=="reflection" || content=="article"){
+            textarea75 = '<div class="textarea-input form-outline w-75 mb-4"><textarea class="form-control" id="textAreaExample4" rows="3" placeholder="Reflect as per given guidelines"></textarea></div>'
+            toRender = '<center>'+textarea75+'</center>';
+        }
+        if(content=="eb"){
+            textarea75 += '<div class="textarea-input form-outline w-75 mb-4"><label class="form-label" for="textAreaExample4">Introduction</label><textarea class="form-control" id="textAreaExample4" rows="3" placeholder="Enter Introduction"></textarea></div><hr>'
+            textarea75 += '<div class="textarea-input form-outline w-75 mb-4"><label class="form-label" for="textAreaExample4">Thread A Example 1</label><textarea class="form-control" id="textAreaExample4" rows="3" placeholder="Enter Text Here"></textarea></div>'
+            textarea75 += '<div class="textarea-input form-outline w-75 mb-4"><label class="form-label" for="textAreaExample4">Argument for Thread A</label><textarea class="form-control" id="textAreaExample4" rows="3" placeholder="Enter Text Here"></textarea></div>'
+            textarea75 += '<button class="btn btn-primary new-thread-btn btn-sm">+ New Thread</button><hr>'
+            textarea75 += '<div class="textarea-input form-outline w-75 mb-4"><label class="form-label" for="textAreaExample4">Conclusion</label><textarea class="form-control" id="textAreaExample4" rows="3" placeholder="Enter Conclusion Here"></textarea></div>'
+            textarea75 += '<button class="btn btn-primary">Save</button>'
+            toRender = '<center>'+textarea75+'</center>';
+        }
+    }
+
+    var file = "";
+    if(type=="display_asset"){
+        if(url!=null && url!=""){
+            // file = '<audio controls="controls"><source src="'+url+'" type="audio/ogg"></audio>';
+            file = '<iframe src="'+url+'" width="100%"></iframe>';
+        }
+        if(video!=null && video!=""){
+            // file += '<video width="500px" height="500px" controls="controls"/> <source src="'+video+'" type="video/mp4"> </video> '
+            // width="420" height="345" 
+            file += '<iframe src="'+video+'" width="100%" height="345px"></iframe>';
+        }
+        if(image!=null && image!=""){
+            file += '<img src="'+image+'" width="100%" alt="">';
+        }
+        if(docs!=null && docs!=""){
+            file += '<iframe src="'+docs+'" width="100%" height="500px"></iframe>'
+        }
+        if(reflection!=null && reflection!=""){
+            file+='<p><strong>'+reflection+'</strong></p>';
+        }
+    }
+    
+    var listing = document.getElementsByClassName('side-menu-list')[0].innerHTML;
+    document.getElementsByClassName('side-menu-list')[0].innerHTML = listing + '<li class="side-menu-item">'+title+'</li><hr>'
+    document.getElementsByClassName('tasks')[0].innerHTML = doc+"<div class='card col-lg-5 col-md-3 col-sm-3'><p class='card-title'><strong>"+title+"</strong></p><div class='card-body'>"+toRender+file+"<hr><div class='description-button'><i class='arrows fa-solid fa-angle-down'></i></div><div class='card-description'>"+description+"</div></div></div>";
+}
+
+document.getElementsByClassName("side-menu-heading")[0].addEventListener("click", function(){
+    // alert("I GOT CLICKED");
+    document.getElementsByClassName("sidemenu")[0].classList.toggle('active');
+});
+
+function descriptionToggler(a){
+    document.getElementsByClassName("description-button")[a].addEventListener("click", function(){
+        document.getElementsByClassName("arrows")[a].classList.toggle('fa-angle-down');
+        document.getElementsByClassName("arrows")[a].classList.toggle('fa-angle-up');
+        document.getElementsByClassName("card-description")[a].classList.toggle('des-active');
+    });
+}
+
+for(var j=0; j<document.querySelectorAll(".description-button").length; j++){
+    descriptionToggler(j);
+}
+
